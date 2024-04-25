b3cf62d51d311fb1647a3825f7b8f2f15413bc1d
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-04-24 23:30:58 -0500

    refactor (qr_gen.py): add error handling and accept url arg

--- boilerplate/qr_gen.py
+++ boilerplate/qr_gen.py
@@ -1,9 +1,42 @@
 #!/usr/bin/env python3
 
+import sys
+from datetime import datetime
 from pathlib import Path
-from pyqrcode import QRCode
+
+try:
+    from pyqrcode import QRCode
+except ImportError:
+    print("pyqrcode module not found. Install it with 'python -m pip install pyqrcode pypng'")
+    sys.exit(1)
 
 home = str(Path.home())
+downloads = Path(f"{home}/Downloads")
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
-url = QRCode("https://github.com/pythoninthegrass/python_template")
-url.png(Path(f"{home}/Downloads/repo.png"), scale=8)
+if len(sys.argv) == 2 and sys.argv[1].startswith("http"):
+    url = QRCode(sys.argv[1])
+else:
+    url = QRCode("https://github.com/pythoninthegrass/python_template")
+
+
+def check_file():
+    """
+    Check if QR code already exists in ~/Downloads folder.
+    If it does, rename it with a timestamp.
+    """
+    if Path(f"{home}/Downloads/qr.png").exists():
+        print("QR code already exists in ~/Downloads")
+        new_file_name = Path(f"{downloads}/qr_{timestamp}.png")
+        Path(f"{downloads}/qr.png").rename(new_file_name)
+        print(f"QR code copied to Downloads folder as 'qr_{timestamp}.png'")
+
+
+def main():
+    check_file()
+    url.png(Path(f"{home}/Downloads/qr.png"), scale=8)
+    print("QR code saved to Downloads folder as 'qr.png'")
+
+
+if __name__ == "__main__":
+    main()
